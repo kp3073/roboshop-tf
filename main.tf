9efc941f53ce8123ee3744b7bbe8daf3c1af9388
kp3073 <keyurpatel8288@gmail.com>
2024-12-03 19:36:37 -0500

    kp | Robosho terraform | s3 backend correction code

--- main.tf
+++ main.tf
@@ -3,7 +3,9 @@ module "vpc" {
   vpc_cidr                   = var.vpc_cidr
   env                        = var.env
   public_subnet              = var.public_subnet
-  private_subnet             = var.private_subnet
+  web_subnet                 = var.web_subnet
+  app_subnet                 = var.app_subnet
+  db_subnet                  = var.db_subnet
   azs                        = var.azs
   default_vpc_id             = var.default_vpc_id
   account_no                 = var.account_no
